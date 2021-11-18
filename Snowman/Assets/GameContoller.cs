cf8a2cbbe1e4db1959e8639cf096ad8d93280e27
JoshGoodman22 <jgoodman2024@chadwickschool.org>
2021-11-18 12:20:50 -0800

    All work from 11/18 with alot of code in GAMECONRT

--- Snowman/Assets/GameContoller.cs
+++ Snowman/Assets/GameContoller.cs
@@ -1,29 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using WordGuesser;
 
 public class GameContoller : MonoBehaviour
 {
     public UnityEngine.UI.Text Message;
     public UnityEngine.UI.Button StartButton;
 
-    public gameObject StartScreen;
+    public GameObject StartScreen;
 
-    public gameObject Playscreen;
+    public GameObject PlayScreen;
+
+    private WordGuesser.WordGame guessingGame;
+    
+    public UnityEngine.UI.InputField PlayerGuess;
+
 
     public void StartGame()
     {
-        this.Message.text = "Can you save the Snowman";
-        this.StartButton.gameObject.SetActive(false);
+        this.guessingGame = new WordGuesser.WordGame("apple", 5);
+        Debug.Log(this.guessingGame.GetWord());
+        Debug.Log(this.guessingGame.GetFullWord());
+        
         this.StartScreen.SetActive(false);
-        this.Playscreen.SetActive(true);
+        this.PlayScreen.SetActive(true);
     }
     
     public void OpenStartScreen()
     {
-        this.StartGame.SetActive(true);
-        this.StartScreen.SetActive(false;
+        this.PlayScreen.SetActive(false);
+        this.StartScreen.SetActive(true);
+    }
+
+    public void Start()
+    {
+        this.StartScreen.SetActive(true);
+        this.PlayScreen.SetActive(false);
     }
     
+    public void SubmitGuess()
+    {
+        Debug.Log(this.guessingGame.CheckGuess(PlayerGuess.text));
+        PlayerGuess.text = string.Empty;
+    }
     
 }
