08ed207c854b5a1b15ffdfd42a9805f5ea3f49a9
JoshGoodman22 <jgoodman2024@chadwickschool.org>
2021-11-17 22:45:24 -0800

    Challange one work

--- Snowman/Assets/GameContoller.cs
+++ Snowman/Assets/GameContoller.cs
@@ -7,9 +7,23 @@ public class GameContoller : MonoBehaviour
     public UnityEngine.UI.Text Message;
     public UnityEngine.UI.Button StartButton;
 
+    public gameObject StartScreen;
+
+    public gameObject Playscreen;
+
     public void StartGame()
     {
         this.Message.text = "Can you save the Snowman";
         this.StartButton.gameObject.SetActive(false);
+        this.StartScreen.SetActive(false);
+        this.Playscreen.SetActive(true);
     }
+    
+    public void OpenStartScreen()
+    {
+        this.StartGame.SetActive(true);
+        this.StartScreen.SetActive(false;
+    }
+    
+    
 }
