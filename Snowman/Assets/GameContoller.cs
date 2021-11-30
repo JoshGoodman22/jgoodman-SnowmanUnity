e0ecdae485c002444710b6d9ea523fde227e1af6
JoshGoodman22 <jgoodman2024@chadwickschool.org>
2021-11-30 10:45:23 -0800

    Work 11/30

--- Snowman/Assets/GameContoller.cs
+++ Snowman/Assets/GameContoller.cs
@@ -8,14 +8,22 @@ public class GameContoller : MonoBehaviour
     public UnityEngine.UI.Text Message;
     public UnityEngine.UI.Text HiddenLetters;
     public UnityEngine.UI.Text LettersGuessed;
+
+    public UnityEngine.UI.Text GuessesRem;
+
+    public UnityEngine.UI.Text GuessInfo;
     public UnityEngine.UI.Button StartButton;
+
+    public UnityEngine.UI.Button PlayAgain;
 
     public GameObject StartScreen;
 
     public GameObject PlayScreen;
 
+    public GameObject GameOverScreen;
+
     private WordGuesser.WordGame guessingGame;
-    
+
     public UnityEngine.UI.InputField PlayerGuess;
 
 
@@ -24,11 +32,11 @@ public class GameContoller : MonoBehaviour
         this.guessingGame = new WordGuesser.WordGame("apple", 5);
         Debug.Log(this.guessingGame.GetWord());
         Debug.Log(this.guessingGame.GetFullWord());
-        
+
         this.StartScreen.SetActive(false);
         this.PlayScreen.SetActive(true);
     }
-    
+
     public void OpenStartScreen()
     {
         this.PlayScreen.SetActive(false);
@@ -37,13 +45,18 @@ public class GameContoller : MonoBehaviour
 
     public void Start()
     {
+        this.GameOverScreen.SetActive(false);
         this.StartScreen.SetActive(true);
         this.PlayScreen.SetActive(false);
     }
-    
+
     public void SubmitGuess()
     {
         Debug.Log(this.guessingGame.CheckGuess(PlayerGuess.text));
+        LettersGuessed.text = this.guessingGame.GetGuessedLetters();
+        GuessesRem.text = $"{this.guessingGame.GetGuessLimit() - this.guessingGame.GetIncorrectGuesses()}";
+        HiddenLetters.text = this.guessingGame.GetWord();
+        GuessInfo.text = this.guessingGame.CheckGuess(PlayerGuess.text);
         PlayerGuess.text = string.Empty;
     }
 }
